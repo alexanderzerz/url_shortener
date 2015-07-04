983e85cf3401b0057fd4610a8a324e31cba403df
alexanderzerz <alexander.zerz@inode.at>
2015-07-04 11:37:42 +0200

    fixes

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -76,5 +76,17 @@ class UrlsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def url_params
       params.require(:url).permit(:url_long, :url_short)
+    end
+
+    # AZ27062016: add address to model data, needed for warning message
+    def set_url_host_model
+      if @url.present?
+        @url.url_host = @url_host
+      end
+    end    
+
+    # AZ27062016: build address here, needs less requests than in the view
+    def set_url_host      
+      @url_host = request.protocol() + request.host_with_port() + '/'             
     end    
 end
--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -1,13 +1,2 @@
 module UrlsHelper
-	# AZ27062016: add address to model data, needed for warning message
-    def set_url_host_model
-      if @url.present?
-        @url.url_host = @url_host
-      end
-    end    
-
-	# AZ27062016: build address here, needs less requests than in the view
-    def set_url_host      
-      @url_host = request.protocol() + request.host_with_port() + '/'             
-    end
 end
